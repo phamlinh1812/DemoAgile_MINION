f020769942dd27fc0ac049826d93d8776cca3586
DANGTHIBICHTRUYEN <80092695+DANGTHIBICHTRUYEN@users.noreply.github.com>
2022-04-04 15:46:56 +0700

    MIN - 54

---
+++ View/Carousel.php
@@ -0,0 +1,70 @@
+<div class="maincontent-area">
+<div class="slider-area">
+    <!-- Slider -->
+
+    <div class="block-slider block-slider4">
+                <ul class="" id="bxslider-home4">
+                    <li>
+                        <a href="index.php?mod=products&act=allproducts">
+                            <img  src="Images/carousel_01.png" alt="Slide">
+                            
+                        </a>
+
+                        <div class="caption-group">
+                            <a class="caption button-radius" href="index.php?mod=products&act=allproducts"><span class="icon"></span>Shop now</a>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="index.php?mod=products&act=allproducts"><img src="Images/carousel_02.png" alt="Slide"></a>
+                        <div class="caption-group">
+                            <a class="caption button-radius" href="index.php?mod=products&act=allproducts"><span class="icon"></span>Shop now</a>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="index.php?mod=products&act=allproducts"><img src="Images/carousel_03.jpg" alt="Slide"></a>
+                        <div class="caption-group">
+                            <a class="caption button-radius" href="index.php?mod=products&act=allproducts"><span class="icon"></span>Shop now</a>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="index.php?mod=products&act=allproducts"><img src="Images/carousel_04.png" alt="Slide"></a>
+                        <div class="caption-group">
+                            <a class="caption button-radius" href="index.php?mod=products&act=allproducts"><span class="icon"></span>Shop now</a>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="index.php?mod=products&act=allproducts"><img src="Images/carousel_05.png" alt="Slide"></a>
+                        <div class="caption-group">
+                            <a class="caption button-radius" href="index.php?mod=products&act=allproducts"><span class="icon"></span>Shop now</a>
+                        </div>
+                    </li>
+                </ul>
+                
+                </div>
+            <!-- ./Slider -->
+          
+            
+     <!-- End slider area -->
+     </div>
+     <!-- <div class="right-slider">
+                     <ul class="" id="bxslider-home4">
+                        <li>
+                        <a href="index.php?mod=products&act=allproducts">
+                            <img  src="Images/tem01.png" alt="Slide"></a>
+                         
+
+                    </li>
+                    <li>
+                        
+                         
+                            <a href="index.php?mod=products&act=allproducts"> 
+                                   <img  src="Images/tem02.png" alt="Slide"></a>
+                        
+
+                    </li>
+                 </ul>
+                </div>
+  -->
+     </div> 
+     </div> 
+
