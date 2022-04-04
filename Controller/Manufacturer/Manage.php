6bdabeee84643efc18050e02cb28f4227c49ca5b
vinhnani <81847897+vinhnani@users.noreply.github.com>
2022-04-04 22:27:38 +0700

    Delete Controller/Manufacturer directory

--- Controller/Manufacturer/Manage.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include_once("Model/Manufacturer.php");
-	$ma = new Manufacturer();
-	$ret = $ma->getManufacturerManage();
-	include_once("View/Manufacturer/Manage.php");
-?>
--- Controller/Manufacturer/Manufacturer.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include_once("Model/Manufacturer.php");
-	$manu = new Manufacturer();
-	$result = $manu->getManufacturer();
-	include_once("View/Manufacturer/Manufacturer.php");
-?>
