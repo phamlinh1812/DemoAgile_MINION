719fcd31caf352491cdeca24fa1590aeacd8e5a3
DANGTHIBICHTRUYEN <80092695+DANGTHIBICHTRUYEN@users.noreply.github.com>
2022-04-04 19:08:36 +0700

    MIN-58

---
+++ View/Products/Search.php
@@ -0,0 +1,4 @@
+<form method="POST" action="index.php?mod=products&act=resultsearch">
+    <input style="margin-top: 10px;margin-left: 20px;border: solid; border-color:#2D2520 ;border-radius: 10px;" type="text" placeholder="Nhập tên sản phẩm/Mã sản phẩm (Barcode)" name="txtSearch">
+    <input style="background-color: #2D2520;border-radius: 10px;"type="submit" name="btnTimKiem" value="Tìm kiếm">
+</form>
