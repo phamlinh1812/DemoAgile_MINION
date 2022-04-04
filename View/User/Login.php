b31f5d689ebf11c820ce2b404235a1ecc609ede9
letham8901 <80095476+letham8901@users.noreply.github.com>
2022-04-04 20:44:18 +0700

    MIN-1

--- View/User/Login.php
+++ View/User/Login.php
@@ -7,8 +7,8 @@
         <p style="font-size: 18px"><label>Tên đăng nhập</label><input type="text" name="txtUserName" id="txtUserName" style="margin-left: 38px;" /></p>
         <p style="font-size: 18px"><label>Mật khẩu  </label><input type="password" name="txtPassWord" id="txtPassWord"  style="margin-left: 88px;" /></p>
         
-        <p><label>&nbsp;</label><input type="submit" name="btnLogin" id="btnLogin" value="Đăng nhập" style="margin-left: 0px;font-weight:bold;background-color: #990033"/>
-        <a style="color: #ff6600;" href="index.php?mod=user&act=forgotpassword"><b><u>Quên mật khẩu?</u></b></a></p>
+        <p><label>&nbsp;</label><input type="submit" name="btnLogin" id="btnLogin" value="Đăng nhập" style="margin-left: 0px;font-weight:bold;background-color: #b05529"/></p>
+        <p><a style="color: #ff6600;" href="index.php?mod=user&act=forgotpassword"><b><u>Quên mật khẩu?</u></b></a></p>
 
         <p style="font-size: 20px; text-align: center; color: red; font-weight: bold;" id="error"></p>
     </form>
